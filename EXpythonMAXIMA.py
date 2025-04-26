6e933ef296dc3f3cde4a5db2b2c14c37d1279961
UnfortunateGames <83056664+UnfortunateGames@users.noreply.github.com>
2025-04-26 10:21:20 +0800

    Updated python file

--- EXpythonMAXIMA.py
+++ EXpythonMAXIMA.py
@@ -1,4 +1,4 @@
-# Organization of The Rewrite
+# ex - PYTHON - maxima
 
 # Import Libraries
 
@@ -43,12 +43,14 @@ GOCsprite = [
     r" | -_- |",
     r" \'.-.'/",
     r"  \___/",
+    
     r"   -=-",
     r".---+---.",
     r"(> ^ ^ <)",
     r" | -_- |",
     r" |'.-.'|",
-    r" \'.-.'/"
+    r" \'.-.'/",
+
     r"   -=-",
     r".---+---.",
     r"(U ^ ^ U)",
@@ -58,21 +60,20 @@ GOCsprite = [
 ]
 
 GOAsprite = [
-    r"    .",
     r"  .' '.",
     r"  |. .|",
     r" ( \./ )",
     r"_=======_",
     r"   | |",
-    r"  ('_')"
-    r"    .",
+    r"  ('_')",
+
     r"  .' '.",
     r"  |. .|",
     r" ( \./ )",
     r"_=======_",
     r"   | |",
-    r"  (>o<)"
-    r"    .",
+    r"  (>o<)",
+
     r"  .' '.",
     r"  |. .|",
     r" ( \./ )",
@@ -118,7 +119,7 @@ castSprite = [
 
 # Your Stats
 
-curlocation = {"locNumber": 0, "locName": "village"}
+curlocation = {"locNumber": 0, "locName": "towns"}
 
 BattleStats = {"HP": 15, "MP": 10, "DMG": 1, "AR": 1, "RNGDMG": 4}
 
@@ -202,57 +203,52 @@ bandit = {
 
 # Locations
 
-locations = {
-    "towns": {
-        "village": {
-            "sprite": villSprite,
-            "rest": "Home",
-            "shop": "Shop",
-            "quest": "Elder",
-            "where": ["Grasslands", "town"],
-        },
-        "town": {
-            "sprite": townSprite,
-            "rest": "Inn",
-            "shop": "Blacksmith",
-            "quest": "Mayor",
-            "where": ["Village", "Castle"],
-        },
-        "castle": {
-            "sprite": castSprite,
-            "tavern": "Tavern",
-            "armory": "Armory",
-            "king": "King",
-            "leave": ["Town", "Wilderness"],
-        },
+towns = {
+    "village": {
+        "sprite": villSprite,
+        "rest": "Home",
+        "shop": "Shop",
+        "quest": "Elder",
+        "where": ["Grasslands", "town"],
     },
-    "wild": {
-        "Grasslands": {
-            "enemies": [rabbit, deer, bandit],
-            "leave": ["village", "Forest"],
-        },
-        "Forest": {
-            "enemies": [], 
-            "leave": ["Grasslands", "Caverns"]
-        },
+    "town": {
+        "sprite": townSprite,
+        "rest": "Inn",
+        "shop": "Blacksmith",
+        "quest": "Mayor",
+        "where": ["Village", "Castle"],
+    },
+    "castle": {
+        "sprite": castSprite,
+        "tavern": "Tavern",
+        "armory": "Armory",
+        "king": "King",
+        "leave": ["Town", "Wilderness"],
     },
 }
+
+wild = {
+    "Grasslands": {
+        "enemies": [rabbit, deer, bandit],
+        "leave": ["village", "Forest"],
+    },
+    "Forest": {
+        "enemies": [], 
+        "leave": ["Grasslands", "Caverns"]
+    },
+},
 
 # Functions
 
 # Essential
 
-
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
-
 
 def wait(n):
     time.sleep(n)
 
-
 # Calculation
-
 
 def calculateStats():
     global LevelMaxStats, BattleStats
@@ -264,16 +260,14 @@ def calculateStats():
     BattleStats["AR"] = level * 1.5
     BattleStats["RNGDMG"] = level * 2 + random.randint(0, 5)
 
-
 # Game Parts
-
 
 def drawTopBar():
     B = PersonalStats["B"] % 100
     S = PersonalStats["B"] // 100 % 100
     G = PersonalStats["B"] // 10000
     name = PersonalStats["Name"]
-    hp = BattleStats["HP"]
+    hp = int(BattleStats["HP"])
     maxhp = LevelMaxStats["MAXHP"]
     mp = BattleStats["MP"]
     maxmp = LevelMaxStats["MAXMP"]
@@ -281,13 +275,14 @@ def drawTopBar():
     exp = PersonalStats["EXP"]
     maxexp = LevelMaxStats["MAXEXP"]
     print("-." * STwidth, end="")
-    print(f"\n\tName: {name}\t\t\texPYTHON")
-    print(f"\tHP: {hp}/{maxhp} MP: {mp}/{maxmp} | [G]-{G} [S]-{S} [B]-{B}\t--------")
-    print(f"\tLV: {level} EXP: {exp}/{maxexp}\t\t\tMAXIMA")
-    print("-." * STwidth, end="", flush=True)
-
+    print(f"\n\tName: {name}\t\t   EX\t  //ESplash")
+    print(f"\tHP: {hp}/{maxhp} MP: {mp}/{maxmp}\t\t PYTHON\t / /__ :)")
+    print(f"\tG]-{G} [S]-{S} [B]-{B}\t\t-.-.-.-./__  / (:")
+    print(f"\tLV: {level} EXP: {exp}/{maxexp}\t\t\t MAXIMA\t  / /  :)")
+    print(".-" * STwidth, end="", flush=True)
 
 def fakeLoadingStuff():
+    # TODO Think of a better loading screen
     clear()
     print("Importing Libraries... ", end="", flush=True)
     wait(1)
@@ -324,7 +319,6 @@ def fakeLoadingStuff():
 
 # Save Files
 
-
 def loadSave():
     global PersonalStats, BattleStats, LevelMaxStats
     try:
@@ -332,16 +326,12 @@ def loadSave():
             data = file.readlines()
             if not data or len(data) < 6:
                 raise ValueError("Save file is empty or corrupted.")
-
             PersonalStats["Name"] = data[0].strip()
             PersonalStats["EXP"] = int(data[1].strip())
             PersonalStats["B"] = int(data[2].strip())
-
             BattleStats["HP"] = int(data[3].strip())
             BattleStats["MP"] = int(data[4].strip())
-
             calculateStats()
-
         print("\n\n\tSave file found!")
         input("\n\t\tContinue?")
     except FileNotFoundError:
@@ -358,7 +348,6 @@ def loadSave():
         input("\n\t\tContinue?")
         firstBattles()
 
-
 def saveGame():
     with open(savefile, "w") as file:
         file.write(f"{PersonalStats['Name']}\n")
@@ -367,7 +356,6 @@ def saveGame():
         file.write(f"{BattleStats['HP']}\n")
         file.write(f"{BattleStats['MP']}\n")
         file.write(f"{PersonalStats['level']}\n")
-
 
 def LOADorSAVE():
     x = input("\n\t\t[ Load a save? Or Start anew? ]\n\t\t>> ( load/new ) ")
@@ -379,9 +367,7 @@ def LOADorSAVE():
         print("\n\n\tInvalid input!")
         LOADorSAVE()
 
-
 # Battle System
-
 
 def HealStats():
     global BattleStats, LevelMaxStats
@@ -389,8 +375,18 @@ def HealStats():
     BattleStats["MP"] = LevelMaxStats["MAXMP"]
 
 def getDream():
-    # this is a placeholder
-    print("\n\n\t")
+    # TODO Work on this feature!
+    print("\n\n\t\tYou walk through the Grasslands")
+    wait(2)
+    print("\t\tYou wonder...")
+    wait(2)
+    print("\t\tIs killing every monster worth it?")
+    wait(2)
+    print("\t\tThen a revelation comes to your mind")
+    wait(2)
+    print("\t\tWhat if...", end="", flush=True)
+    wait(1)
+    print("\t\tEverything is...")
 
 def restPlace(n):
     if n == 0:
@@ -425,17 +421,15 @@ def restPlace(n):
     if n == 2:
         pass
 
-
 def drawLocation():
-    town = locations["towns"]
     # wild = locations["wild"]
     if curlocation["locName"] == "village":
         if curlocation["locNumber"] == 0:
-            loc = town["village"]
+            loc = towns["village"]
         elif curlocation["locNumber"] == 1:
-            loc = town["town"]
+            loc = towns["town"]
         elif curlocation["locNumber"] == 2:
-            loc = town["castle"]
+            loc = towns["castle"]
     elif curlocation["locName"] == "wild":
         if curlocation["locNumber"] == 0:
             pass
@@ -473,7 +467,6 @@ def drawLocation():
             clear()
             drawLocation()
 
-
 def loadEnemy(enemy, i=0):
     hp = enemy["HP"]
     maxhp = enemy["MAXHP"]
@@ -486,7 +479,6 @@ def loadEnemy(enemy, i=0):
             print("\t\t\t" + localSprite[sprite])
     print(f"\n\t\t    [ HP ] {hp} / {maxhp}")
 
-
 def respawnSystem(r, enemy):
     global curlocation
     if not r:
@@ -497,6 +489,7 @@ def respawnSystem(r, enemy):
         wait(3)
         input("\t\t\tPress enter to retry... ")
         HealStats()
+        enemy["HP"] = enemy["MAXHP"]
         BattleSystem(enemy, r)
     else:
         curlocation["locName"] = "village"
@@ -507,7 +500,6 @@ def respawnSystem(r, enemy):
         print("\t\t\tIt all starts anew...")
         wait(3)
         input("\t\t\tPress enter to respawn... ")
-        
 
 def enemyChoice(enemy, r):
     enemyRNG = random.randint(0, 100)
@@ -517,7 +509,7 @@ def enemyChoice(enemy, r):
         BattleStats["HP"] -= donedmg
         clear()
         loadEnemy(enemy)
-        print(f"\n\n\tThe enemy dealt {donedmg} damage!")
+        print(f"\n\n\tThe enemy dealt {int(donedmg)} damage!")
         wait(2)
         if BattleStats["HP"] <= 0:
             respawnSystem(r, enemy)
@@ -536,31 +528,27 @@ def enemyChoice(enemy, r):
         print("\n\n\tThe enemy is casting a spell!")
         wait(1)
         enemyDMG = enemy["DMG"] * random.randint(0, 2)
+        BattleStats["HP"] -= enemyDMG
         clear()
         loadEnemy(enemy)
-        print(f"\n\n\tThe enemy dealt {enemyDMG} damage!")
+        print(f"\n\n\tThe enemy dealt {int(enemyDMG)} damage!")
         wait(2)
         if BattleStats["HP"] <= 0:
             respawnSystem()
         else:
             BattleSystem(enemy, r)
 
-
 def LevelupSystem():
     clear()
-    global PersonalStats, BattleStats, LevelMaxStats
-    level = PersonalStats["level"]
-    exp = PersonalStats["EXP"]
-    maxexp = LevelMaxStats["MAXEXP"]
-    if exp >= maxexp:
-        exp = 0
-        level += 1
+    global PersonalStats, LevelMaxStats
+    if PersonalStats["EXP"] >= LevelMaxStats["MAXEXP"]:
+        PersonalStats["EXP"] = 0
+        PersonalStats["level"] += 1
         calculateStats()
         drawTopBar()
         print("\n\n\t\t\tYou leveled up!")
         wait(2)
         input("\n\t\t\tPress enter to continue...")
-
 
 def rewardSystem(enemy):
     clear()
@@ -573,7 +561,6 @@ def rewardSystem(enemy):
     wait(1)
     input("\n\tPress Enter to continue...")
     LevelupSystem()
-
 
 def BattleSystem(enemy, r=True):
     global BattleStats
@@ -637,7 +624,6 @@ def BattleSystem(enemy, r=True):
         wait(2)
         BattleSystem(enemy, r)
 
-
 # Story Related
 
 def firstBattles():
@@ -654,6 +640,7 @@ def firstBattles():
     clear()
     drawTopBar()
     print("\n\n\t\tYou have advanced through your life.")
+    HealStats()
     wait(2)
     print("\t\tThe second Guardian is approaching.")
     wait(2)
@@ -663,14 +650,16 @@ def firstBattles():
     clear()
     drawTopBar()
     print("\n\n\t\tYou progressed through childhood.")
+    HealStats()
     wait(2)
-    print("\n\t\tLonging for adventure, you meet the final Guardian.")
+    print("\n\t\tLonging for adventure")
     wait(2)
-    print("\t\tThe Guardian of Adventure challenges you to test your skills.")
-    wait(1)
-    input("\n\t\tYou stand for one final trial, press enter to battle!")
+    print("\t\tYou meet the final Guardian.")
+    wait(2)
+    print("\n\t\tIt challenges you to test your skills.")
+    wait(2)
+    input("\n\t\tYou stand for one final trial\n\n\t\tPress enter to battle!")
     BattleSystem(GuardianOfAdventure, False)
-
 
 def initIntroSequence():
     clear()
@@ -693,22 +682,21 @@ def initIntroSequence():
         wait(0.02)
     print("\n\n\n")
     for _ in range(0, STwidth):
-        print("-", end="", flush=True)
-        wait(0.02)
         print(".", end="", flush=True)
+        wait(0.02)
+        print("-", end="", flush=True)
         wait(0.02)
     clear()
     drawTopBar()
-    wait(1)
+    wait(2)
     print("\n\n\t\tYou are about to enter")
     print("\t\tthe world of MAXIMA.")
     wait(2)
     print("\n\t\tThere are many souls to pick.")
-    wait(1)
+    wait(2)
     print("\t\tLet us find, a soul fit for you.")
-    wait(1)
+    wait(3)
     LOADorSAVE()
-
 
 # Run Code
 
